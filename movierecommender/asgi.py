af50ceef72f39cfdb90126eb5a5934a76d09a348
wey1998 <yitwey1998@hotmail.com>
2023-05-13 01:37:22 +0100

    restart project

--- movierecommender/asgi.py
+++ movierecommender/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for movie_recommender project.
+ASGI config for movierecommender project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_recommender.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierecommender.settings')
 
 application = get_asgi_application()
