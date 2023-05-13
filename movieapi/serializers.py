2152548c0d55641c145b1aa67f7520527d936b63
wey1998 <yitwey1998@hotmail.com>
2023-05-13 17:52:19 +0100

    Add serializer for all models

---
+++ movieapi/serializers.py
@@ -0,0 +1,70 @@
+from rest_framework import serializers
+
+from .models import Actor, CustomUser, Director, Movie, RatedMovies
+
+
+class MovieMiniSerializer(serializers.Serializer):
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "genre", "language", "published_date"]
+
+
+class DirectorSerializer(serializers.Serializer):
+    class Meta:
+        model = Director
+        fields = ["id", "name"]
+
+
+class ActorSerializer(serializers.Serializer):
+    class Meta:
+        model = Actor
+        fields = [
+            "id",
+            "name",
+        ]
+
+
+class MovieSerializer(serializers.Serializer):
+    director = DirectorSerializer(many=False)
+    actor = ActorSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = [
+            "id",
+            "title",
+            "genre",
+            "language",
+            "published_date",
+            "metascore",
+            "duration",
+            "director",
+            "actor",
+        ]
+
+
+class CustomUserMiniSerializer(serializers.Serializer):
+    model = CustomUser
+    fields = ["id", "username", "country"]
+
+
+class RatedMovieMiniSerializer(serializers.Serializer):
+    class Meta:
+        model = RatedMovies
+        fields = ['id', "movie", "userRating"]
+
+
+class CustomUserSerializer(serializers.Serializer):
+    user_rated_movies = RatedMovieMiniSerializer(many=False)
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "preferred_name",
+            "username",
+            "email",
+            "age",
+            "country",
+            "user_rated_movies",
+        ]
