fc31b16eeca3956f3420a9fb2aa3a5601bbe4216
wey1998 <yitwey1998@hotmail.com>
2023-05-16 02:35:38 +0100

    Add loop, router and contidion tutorial

--- tutorial-app/src/components/footer.jsx
+++ tutorial-app/src/components/footer.jsx
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <h5>This is the footer {this.props.trademark}</h5>;
+    const animals = ["horse", "dog", "cat"];
+
+    return (
+      <React.Fragment>
+        <h5>This is the footer {this.props.trademark}</h5>
+        <p>Passing message from header {this.props.userInput}</p>
+        <p>
+          {this.props.userInput !== ""
+            ? "Footer has been updated"
+            : "Enter text into text box and submit"}
+        </p>
+        <ul>
+          {animals.map((animal) => {
+            return (
+              <React.Fragment key={animal}>
+                <li>{animal}</li>
+              </React.Fragment>
+            );
+          })}
+        </ul>
+      </React.Fragment>
+    );
   }
 }
 
