395a96909af8b5aa7edd4ab2948949a80611c335
wey1998 <yitwey1998@hotmail.com>
2023-06-05 01:23:15 +0100

    update branch

--- movie-frontend/src/App.jsx
+++ movie-frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import { Fragment, useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.jsx";
-import MoiveCardSection from "./components/MovieCardSection";
+import MovieCardSection from "./components/MovieCardSection";
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
   return (
     <Fragment>
-      <Header />
-      <div className="main-content">
-        <MoiveCardSection />
-      </div>
+      <Router>
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<MovieCardSection />} />
+            <Route path="/login" element={<LoginForm />} />
+          </Routes>
+        </div>
+      </Router>
     </Fragment>
   );
 }
