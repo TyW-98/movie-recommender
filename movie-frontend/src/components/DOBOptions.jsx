e265a26374251f5cf63cbef2f08a65c8e2380e20
wey1998 <yitwey1998@hotmail.com>
2023-06-06 01:57:19 +0100

    Add styling to DOB elements

--- movie-frontend/src/components/DOBOptions.jsx
+++ movie-frontend/src/components/DOBOptions.jsx
@@ -13,40 +13,31 @@ export default function DOBOptions(props) {
   return (
     <div className="dob-container">
       <div className="dob-day-container">
-        <label>
-          Day
-          <select value={props.dateOfBirth.day}>
-            <option value="">Day</option>
-            {Array.from({ length: 31 }, (_, index) => index + 1).map((day) => {
-              return (
-                <option key={day} value={day}>
-                  {day}
-                </option>
-              );
-            })}
-          </select>
-        </label>
+        <select value={props.dateOfBirth.day} onChange={handleDOB}>
+          <option value="">Day</option>
+          {Array.from({ length: 31 }, (_, index) => index + 1).map((day) => {
+            return (
+              <option key={day} value={day}>
+                {day}
+              </option>
+            );
+          })}
+        </select>
       </div>
       <div className="dob-month-container">
-        <label>
-          Month
-          <select value={props.dateOfBirth.month}>
-            <option value="">Month</option>
-            {Array.from({ length: 12 }, (_, index) => index + 1).map(
-              (month) => {
-                return (
-                  <option key={month} value={month}>
-                    {month}
-                  </option>
-                );
-              }
-            )}
-          </select>
-        </label>
+        <select value={props.dateOfBirth.month} onChange={handleDOB}>
+          <option value="">Month</option>
+          {Array.from({ length: 12 }, (_, index) => index + 1).map((month) => {
+            return (
+              <option key={month} value={month}>
+                {month}
+              </option>
+            );
+          })}
+        </select>
       </div>
       <div className="dob-year-container">
-        <label>Year</label>
-        <select value={props.dateOfBirth.year}>
+        <select value={props.dateOfBirth.year} onChange={handleDOB}>
           <option value="">Year</option>
           {Array.from({ length: 100 }, (_, index) => currrentYear - index).map(
             (year) => {
